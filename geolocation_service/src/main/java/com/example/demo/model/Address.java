7d2097b9f31a206cef13c40adc01a9495a7dc533
karabutov <32238762+karabutov@users.noreply.github.com>
2020-09-21 09:06:48 +0100

    Add files via upload

---
+++ geolocation_service/src/main/java/com/example/demo/model/Address.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.*;
+
+@Setter
+@Builder
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    private String address;
+
+}
